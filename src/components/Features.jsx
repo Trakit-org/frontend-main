940776ad65e017e3b31a3c7097bce45b7935da77
Donkor James <jamesdonkor987@gmail.com>
2025-01-27 12:34:06 +0000

    updated codebase

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,4 +1,4 @@
-import { FaPencilAlt, FaBell, FaCalendar } from 'react-icons/fa';
+import { FaPencilAlt, FaBell, FaCalendar, FaWallet } from 'react-icons/fa';
 
 const Features = () => {
   return (
@@ -11,13 +11,6 @@ const Features = () => {
         </div>
           <h3 className="text-xl font-semibold mb-2">Manual Subscription Management</h3>
           <p className="text-lg text-gray-600">Input details such as service name, cost, renewal date, and notes.</p>
-        </div>
-        <div className="w-full md:w-1/3 xl:w-1/3 p-6 shadow-md">
-            <div className=" flex w-20 h-20 p-2 shadow-lg rounded-full justify-center items-center mb-4">
-                <FaBell className="w-full text-4xl text-blue-500" />
-            </div>
-          <h3 className="text-xl font-semibold mb-2">Reminders</h3>
-          <p className="text-lg text-gray-600">Automated email or in-app notifications for upcoming renewals.</p>
         </div>
         <div className="w-full md:w-1/3 xl:w-1/3 p-6 shadow-md">
             <div className=" flex w-20 h-20 p-2 shadow-lg rounded-full justify-center items-center mb-4">
@@ -34,6 +27,13 @@ const Features = () => {
           <h3 className="text-xl font-semibold mb-2">Reminders</h3>
           <p className="text-lg text-gray-600">Automated email or in-app notifications for upcoming renewals.</p>
         </div>
+        <div className="w-full md:w-1/3 xl:w-1/3 p-6 shadow-md">
+  <div className="flex w-20 h-20 p-2 shadow-lg rounded-full justify-center items-center mb-4">
+    <FaWallet className="w-full text-4xl text-blue-500" />
+  </div>
+  <h3 className="text-xl font-semibold mb-2">Expense Tracking</h3>
+  <p className="text-lg text-gray-600">Easily track your expenses on subscriptions and stay on top of your finances.</p>
+</div>
       </div>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
             <div className='home-hero flex items-center h-[70vh]'>
                 <div className='md:px-20 px-6 space-y-5 md:w-3/5'> 
                     <h1 className='text-3xl md:text-4xl font-bold'>Managing Subscriptions Made Easy</h1>
-                    <p className='text-lg text-gray-600'>Trackit is a subscription analytics tool that leverages built-in charts to provide actionable insights for informed decision making. With Trackit, businesses can easily track and analyze their subscription data to optimize their revenue streams.                    </p>
+                    <p className='text-lg text-gray-600'>Trakit is a subscription analytics tool that leverages built-in charts to provide actionable insights for informed decision making. With Trackit, businesses can easily track and analyze their subscription data to optimize their revenue streams.                    </p>
                     <button className='px-5 py-2 bg-blueish-100 text-white rounded-md'>Get Started</button>
                 </div>
             </div>
